a6ffb52853a742505faab2d7d9328096d120fc43
Predix Robot <predix-robot@ge.com>
2017-09-08 10:46:40 -0700

    Release 1.0.60

--- server/routes/mock-asset.js
+++ server/routes/mock-asset.js
@@ -5,21 +5,17 @@ module.exports = function() {
   // mock asset data contains an extra "filter" property, so we can easily match the Predix API.
   const routes = {};
 
-  // http://localhost:5000/mock-api/predix-asset/asset?filter=group=/group/plant-richmond-refinery
-  // const compressorJson = require(path.resolve(__dirname, '../sample-data/predix-asset/compressor-2017.json'));
-  // routes["asset"] = compressorJson;
-
-  // http://localhost:5000/mock-api/predix-asset/group?filter=parent=/group/enterprise-predix
-  // const groupsJson = require(path.resolve(__dirname, '../sample-data/predix-asset/groups.json'));
-  // routes["group"] = groupsJson;
-
   // https://predix-asset.run.aws-usw02-pr.ice.predix.io/group?filter=users%3Dbd9f70a3-8aaa-490b-b2a8-91ba59e58f0f%3CgroupRef
   const groupsJson = require(path.resolve(__dirname, '../sample-data/predix-asset/groups-for-user.json'));
-  routes["group"] = groupsJson;
+  routes["devicegroup"] = groupsJson;
 
   // https://predix-asset.run.aws-usw02-pr.ice.predix.io/device?filter=groupRef=/group/testcomp2
   const devicesJson = require(path.resolve(__dirname, '../sample-data/predix-asset/devices-in-group.json'));
   routes["device"] = devicesJson;
+
+  // https://predix-asset.run.aws-usw02-pr.ice.predix.io/industrial-asset
+  const industrialAssets = require(path.resolve(__dirname, '../sample-data/predix-asset/kit-asset-model.json'));
+  routes["industrial-asset"] = industrialAssets;
 
   return routes;
 };
